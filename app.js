8f90d96f613bc1bc0b45b144560f0e4f79455d07
Aidan Casey <aidancasey@gmail.com>
2012-07-18 16:22:44 +1000

    removing Jade templating engine
    Signed-off-by: Aidan Casey <aidancasey@gmail.com>

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
-
 /**
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes');
+var express = require('express')  ;
+  //  , routes = require('./routes');
 
 var app = module.exports = express.createServer();
 
 // Configuration
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+    app.set('views', __dirname + '/views');
+   // app.set('view engine', 'jade');     //screw Jade we don't need a templating engine !
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+   // app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 });
 
 app.helpers({
@@ -34,16 +33,21 @@ app.helpers({
 
 // Routes
 
-app.get('/', routes.site.index);
 
-app.get('/users', routes.users.list);
+app.get('/', function (req, res) {
+    console.log("reguest for / came in");
+    res.sendfile(__dirname + '/views/index.html');
+});
+
+
+/*app.get('/users', routes.users.list);
 app.post('/users', routes.users.create);
 app.get('/users/:id', routes.users.show);
 app.post('/users/:id', routes.users.edit);
 app.del('/users/:id', routes.users.del);
 
 app.post('/users/:id/follow', routes.users.follow);
-app.post('/users/:id/unfollow', routes.users.unfollow);
+app.post('/users/:id/unfollow', routes.users.unfollow);*/
 
 app.listen(process.env.PORT || 3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
