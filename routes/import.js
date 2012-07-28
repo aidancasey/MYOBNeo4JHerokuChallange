3321df523a7b8bf5de861ec607cb663e10ecf7bc
aidancasey <aidancasey@gmail.com>
2012-07-28 20:14:29 +1000

    removing comments from inport.js
    Signed-off-by: aidancasey <aidancasey@gmail.com>

--- routes/import.js
+++ routes/import.js
@@ -8,14 +8,14 @@ var businesses = require('../models/business');
 //TO DO move this logic from the route to a specific model - e.g. batchimportmodel...
 
 exports.importFiles = function(req, res, next){
-//    individuals.LoadFromFile();
-//    console.log("individuals added to db");
+    individuals.LoadFromFile();
+    console.log("individuals added to db");
 
-//    employees.LoadFromFile();
-//    console.log("employees added db");
+    employees.LoadFromFile();
+    console.log("employees added db");
 
-//    partners.LoadFromFile();
-//    console.log("partners added db");
+    partners.LoadFromFile();
+    console.log("partners added db");
 
     businesses.LoadFromFile();
     console.log("businesses added db");
